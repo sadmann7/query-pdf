d3cc7854afae57650f61182f924aa2ed1d492198
sadmann7 <sadmann898@gmail.com>
2023-04-08 19:42:35 +0600

    functional sidebar nav

--- src/components/layouts/chat-layout.tsx
+++ src/components/layouts/chat-layout.tsx
@@ -13,12 +13,19 @@ export function ChatLayout({ children }: LayoutProps) {
   return (
     <>
       <Meta />
-      <div className="relative flex h-full w-full overflow-hidden">
-        <div className="flex h-screen max-w-full flex-1 flex-col">
+      <div className="flex h-full w-full overflow-hidden">
+        <main className="flex h-screen max-w-full flex-1 flex-col">
           <SiteHeader />
-          {children}
+          <div className="relative flex h-full w-full flex-1 flex-col items-stretch overflow-hidden md:grid md:grid-cols-[220px_minmax(0,1fr)] md:items-start md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+            <aside className="w-50 fixed left-2.5 z-30 hidden h-full w-52 shrink-0 overflow-y-auto border-r border-r-slate-100 dark:border-r-slate-700 md:sticky md:block">
+              <ScrollArea className="h-full pr-6 lg:py-7">
+                <SidebarNav items={chatConfig.sidebarNav} />
+              </ScrollArea>
+            </aside>
+            {children}
+          </div>
           <SiteFooter />
-        </div>
+        </main>
       </div>
     </>
   )
